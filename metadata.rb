691b8711884c3a623fb39e9f69fa14b9efa12542
Renaud Tircher <renaud.tircher@gmail.com>
2013-05-19 12:48:29 +0800

    Initial version for installing Leiningen

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Renaud Tircher"
+license          "Apache 2.0"
+description      "Installs Leiningen for building Clojure projects."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1.0"
+platforms
+depends "java"
