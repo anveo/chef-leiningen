3ecbf3805e3bed194822cea5ea388c50c4b96d71
Brian Racer <bracer@gmail.com>
2013-07-22 14:14:28 -0600

    disable system-wide install

--- recipes/default.rb
+++ recipes/default.rb
@@ -29,25 +29,25 @@ remote_file "/usr/local/bin/lein" do
   not_if "grep -qx 'export LEIN_VERSION=\"#{node[:leiningen][:version]}\"' /usr/local/bin/lein"
 end
 
-ruby_block "lein-system-wide" do
-  block do
-    rc = Chef::Util::FileEdit.new("/usr/local/bin/lein")
-    rc.search_file_replace_line(/^LEIN_JAR=.*/, "LEIN_JAR=#{jar_file}")
-    rc.write_file
-  end
-  action :nothing
-end
-
-directory node[:leiningen][:jar_dir] do
-  owner node[:leiningen][:user]
-  group node[:leiningen][:group]
-  mode "0744"
-  action :create
-end
-
-execute "lein_self_install" do
-  command "export LEIN_ROOT=1; lein self-install"
-  user   node[:leiningen][:user] 
-  group  node[:leiningen][:group]
-  not_if{ File.exists?(jar_file) }
-end
+#ruby_block "lein-system-wide" do
+#  block do
+#    rc = Chef::Util::FileEdit.new("/usr/local/bin/lein")
+#    rc.search_file_replace_line(/^LEIN_JAR=.*/, "LEIN_JAR=#{jar_file}")
+#    rc.write_file
+#  end
+#  action :nothing
+#end
+#
+#directory node[:leiningen][:jar_dir] do
+#  owner node[:leiningen][:user]
+#  group node[:leiningen][:group]
+#  mode "0744"
+#  action :create
+#end
+#
+#execute "lein_self_install" do
+#  command "export LEIN_ROOT=1; lein self-install"
+#  user   node[:leiningen][:user] 
+#  group  node[:leiningen][:group]
+#  not_if{ File.exists?(jar_file) }
+#end
